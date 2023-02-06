91c149dd75870bfd4830e737514ad6bc4be69055
Mateus Santos <mvsantos013@gmail.com>
2023-02-06 13:51:40 -0300

    feat: add ORM models

--- src/models.py
+++ src/models.py
@@ -2,6 +2,60 @@ from dynamorm import DynaModel
 from marshmallow import Schema, fields
 from src.constants import SERVICE_NAME, ENV
 
+class PermissionModel(DynaModel):
+    class Table:
+        name = f'{SERVICE_NAME}-{ENV}-permissions'
+        hash_key = 'id'
+
+    class PermissionSchema(Schema):
+        id = fields.Str(description='Permission ID')
+
+    class Schema(PermissionSchema):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+class GroupPermissionModel(DynaModel):
+    class Table:
+        name = f'{SERVICE_NAME}-{ENV}-groups-permissions'
+        hash_key = 'groupId'
+        range_key = 'permissionId'
+
+    class GroupPermissionSchema(Schema):
+        groupId = fields.Str(description='Group ID')
+        permissionId = fields.Str(description='Permission ID')
+
+    class Schema(GroupPermissionSchema):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+
+class AllowedDomainModel(DynaModel):
+    class Table:
+        name = f'{SERVICE_NAME}-{ENV}-allowed-domains'
+        hash_key = 'domain'
+
+    class AllowedDomainSchema(Schema):
+        domain = fields.Str(description='Domain')
+
+    class Schema(AllowedDomainSchema):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+
+class DepartmentModel(DynaModel):
+    class Table:
+        name = f'{SERVICE_NAME}-{ENV}-departments'
+        hash_key = 'id'
+
+    class DepartmentSchema(Schema):
+        id = fields.Str(description='Department ID')
+        name = fields.Str(description='Department name')
+
+    class Schema(DepartmentSchema):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+
 class ProfessorModel(DynaModel):
     class Table:
         name = f'{SERVICE_NAME}-{ENV}-professors'
@@ -12,9 +66,10 @@ class ProfessorModel(DynaModel):
         id = fields.Str(description='Professor ID')
         departmentId = fields.Str(description='Department ID')
         name = fields.Str(description='Professor name')
-        description = fields.Str(description='Professor description')
-        about = fields.Str(description='Professor extended description')
-        pictureUrl = fields.Str(description='S3 URI picture', allow_none=True)
+        description = fields.Str(description='Professor description', allow_none=True, default='')
+        about = fields.Str(description='Professor extended description', allow_none=True, default='')
+        pictureUrl = fields.Str(description='S3 URI picture', allow_none=True, default='')
+        publicRating = fields.Bool(description='Public rating', default=False)
 
     class Schema(ProfessorSchema):
         def __init__(self, *args, **kwargs):
